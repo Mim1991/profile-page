1ff3ee6f2517f5777f11ee728d6bf83be294bf2c
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-01-17 22:43:41 +0000

    refactored js code

--- js/card.js
+++ js/card.js
@@ -1,5 +1,4 @@
 //Movement Animation to happen
-// const card = document.querySelector(".card-middle");
 const cardMiddle = document.querySelector(".card-middle");
 const cardLeft = document.querySelector(".card-left");
 const cardRight = document.querySelector(".card-right");
@@ -9,8 +8,6 @@ const webImg = document.querySelector(".circle-middle img");
 const readMoreBtn = document.querySelector(".read-middle a");
 const description = document.querySelector(".info-middle p");
 
-// const sizes = document.querySelector(".sizes");
-
 //Moving Animation Event
 const animateMiddleCard = (card) => {
   card.addEventListener("mousemove", (e) => {
@@ -18,7 +15,30 @@ const animateMiddleCard = (card) => {
     let yAxis = (window.innerHeight / 2 - e.pageY + 1480) / 20;
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
   });
-}
+};
+
+const animateLeft = (card) => {
+  let cardCenter = card.offsetWidth / 2;
+  let cardMidPoint = card.getBoundingClientRect().left + cardCenter;
+
+  card.addEventListener("mousemove", (e, side) => {
+    let xAxis = (cardMidPoint - e.pageX + window.innerWidth / 18) / 4;
+    let yAxis = (window.innerHeight / 2 - e.pageY + 1480) / 20;
+    console.log(window.innerWidth);
+    cardLeft.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  });
+};
+
+const animateRight = (card) => {
+  let cardRightCenter = card.offsetWidth / 2;
+  let cardRightMiddle = card.getBoundingClientRect().left + cardRightCenter;
+
+  card.addEventListener("mousemove", (e) => {
+    let xAxis = (cardRightMiddle - e.pageX - window.innerWidth / 20) / 4;
+    let yAxis = (window.innerHeight / 2 - e.pageY + 1480) / 20;
+    cardRight.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+  });
+};
 
 const animateCardInOut = (card) => {
   card.addEventListener("mouseenter", (e) => {
@@ -28,57 +48,13 @@ const animateCardInOut = (card) => {
     card.style.transition = "all 0.5s ease";
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
   });
-}
-  
+};
 
 if (window.innerWidth > 850) {
-  animateCard(cardMiddle);
-  // animateCard(cardLeft);
-
-
-//  Left Card
-// half the width of the card
-let cardCenter = cardLeft.offsetWidth / 2;
-// distance from edge of screen to mid point of card
-let cardLeftMiddle = cardLeft.getBoundingClientRect().left + cardCenter;
-
-console.log(cardLeft.getBoundingClientRect);
-window.addEventListener("mousemove", (e) => {
-  // console.log(e.pageX);
-});
-
-cardLeft.addEventListener("mousemove", (e) => {
-  let xAxis = (cardLeftMiddle - e.pageX + window.innerWidth / 18) / 4;
-  let yAxis = (window.innerHeight / 2 - e.pageY + 1480) / 20;
-  console.log(window.innerWidth);
-  cardLeft.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-});
-
-cardLeft.addEventListener("mouseenter", (e) => {
-  cardLeft.style.transition = "all 0.05s ease";
-});
-//Animate Out
-cardLeft.addEventListener("mouseleave", (e) => {
-  cardLeft.style.transition = "all 0.5s ease";
-  cardLeft.style.transform = `rotateY(0deg) rotateX(0deg)`;
-});
-
-// Right Card
-let cardRightCenter = cardRight.offsetWidth / 2;
-let cardRightMiddle = cardRight.getBoundingClientRect().left + cardRightCenter;
-
-cardRight.addEventListener("mousemove", (e) => {
-  let xAxis = (cardRightMiddle - e.pageX - window.innerWidth / 20) / 4;
-  let yAxis = (window.innerHeight / 2 - e.pageY + 1480) / 20;
-  console.log(window.innerWidth);
-  cardRight.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
-});
-
-cardRight.addEventListener("mouseenter", (e) => {
-  cardRight.style.transition = "all 0.05s ease";
-});
-//Animate Out
-cardRight.addEventListener("mouseleave", (e) => {
-  cardRight.style.transition = "all 0.5s ease";
-  cardRight.style.transform = `rotateY(0deg) rotateX(0deg)`;
-});
+  animateMiddleCard(cardMiddle);
+  animateRight(cardRight);
+  animateLeft(cardLeft);
+  animateCardInOut(cardRight);
+  animateCardInOut(cardLeft);
+  animateCardInOut(cardMiddle);
+}
